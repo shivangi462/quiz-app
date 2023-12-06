a58cc0598ee60e565503b73cbafb322fcd3e0ee7
shivangi_singh <93123654+shivangi462@users.noreply.github.com>
2023-12-06 09:04:14 +0530

    first commit

---
+++ js/questions.js
@@ -0,0 +1,115 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "With what data structure can a priority queue be implemented?",
+    answer: "Heap",
+    options: [
+        "Array",
+        "List",
+        "Heap",
+        "Tree",
+    ]
+  },
+    {
+    numb: 2,
+    question: "Which one of the below mentioned is linear data structure ?",
+    answer: "All of the above",
+    options: [
+       "Queue",  
+       "Stack",
+       "Arrays",
+       "All of the above",
+    ]
+  },
+    {
+    numb: 3,
+    question: "Program with highest run-time complexity is-",
+    answer: "Tower of Hanoi",
+    options: [
+      "Tower of Hanoi",
+      "Fibonacci Series",
+      "Prime Number Series",
+      "None of the above",
+    ]
+  },
+    {
+    numb: 4,
+    question: "What data structure is used when converting an infix notation to prefix notation?",
+    answer: "Stack",
+    options: [
+      "Stack",
+      "Queue",
+      "B-Trees",
+      "Linked-list",
+    ]
+  },
+    {
+    numb: 5,
+    question: " Which data structure can be used to test a palindrome?",
+    answer: "Stack",
+    options: [
+      "Tree",
+      "Heap",
+      "Stack",
+      "Priority queue",
+    ]
+  },
+
+    {
+    numb: 6,
+    question: "What would be the asymptotic time complexity to find an element in the linked list?",
+    answer: "O(n)",
+    options: [
+        "O(1)",
+        "O(n)",
+        "O(n2)",
+        "O(n4)",
+    ]
+  },
+  {
+    
+    numb: 7,
+    question: "Which of the following is not the type of queue?",
+    answer: "Single ended queue",
+    options: [
+      "Ordinary queue",
+      "Single ended queue",
+      "Circular queue",
+      "Priority queue",
+    ]
+  },
+  {
+    numb: 8,
+    question: "The data structure required for Breadth First Traversal on a graph is?",
+    answer: "Queue",
+    options: [
+        "Stack",
+        "Array",
+        "Queue",
+        "Tree",
+    ]
+  },
+  {
+    numb: 9,
+    question: "Q-9: What is the value of the postfix expression 6 3 2 4 + – *?",
+    answer: "-18",
+    options: [
+      "1",
+      "40",
+      "74",
+      "-18",
+    ]
+  },
+  {
+    numb: 10,
+    question: "How do you initialize an array in C?",
+    answer: "int arr[3] = {1,2,3};",
+    options: [
+     "int arr[3] = (1,2,3);",
+     "int arr(3) = {1,2,3};",
+     "int arr[3] = {1,2,3};",
+     "int arr(3) = (1,2,3);"
+    ]
+  },
+];
